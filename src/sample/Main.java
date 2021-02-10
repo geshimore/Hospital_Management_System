999edfd2e430c7ba2e667f6ee4c4372084ec00c8
Geshimore <peter.ngugi@mail.com>
2021-02-11 00:24:51 +0300

    updates

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
         primaryStage.setTitle("Login Page");
         primaryStage.setScene(new Scene(root, 800, 500));
